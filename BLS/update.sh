f72b17f5d18a1e94ffecb07a281b538ef2a244b8
Kyle <kyle201817146@gmail.com>
2025-09-13 17:56:32 +0800

    Add BacklightServices framework

---
+++ BLS/update.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# A `realpath` alternative using the default C implementation.
+filepath() {
+  [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+VERSION=${DARWIN_PRIVATE_FRAMEWORKS_TARGET_RELEASE:-2024}
+FRAMEWORK_ROOT="$(dirname $(filepath $0))/$VERSION"
+
+framework_name=BacklightServices
+
+generate_framework() {
+    local framework_name=$1
+    local arch_name=$2
+
+    local path="${FRAMEWORK_ROOT}/${framework_name}.xcframework/${arch_name}/${framework_name}.framework"
+    mkdir -p ${path}
+
+    rm -rf ${path}/${framework_name}.tbd
+    rm -rf ${path}/Info.plist
+
+    cp ${FRAMEWORK_ROOT}/tbds/${arch_name}/${framework_name}.tbd ${path}/
+
+    # Create minimal Info.plist for framework
+    cat > ${path}/Info.plist << EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>CFBundleIdentifier</key>
+    <string>com.apple.BacklightServices</string>
+    <key>CFBundleName</key>
+    <string>BacklightServices</string>
+    <key>CFBundlePackageType</key>
+    <string>FMWK</string>
+</dict>
+</plist>
+EOF
+}
+
+generate_xcframework() {
+    local framework_name=$1
+
+    local path="${FRAMEWORK_ROOT}/${framework_name}.xcframework"
+    mkdir -p ${path}
+    cp ${FRAMEWORK_ROOT}/Info.plist ${path}/
+}
+
+generate_xcframework $framework_name
+
+generate_framework $framework_name ios-arm64-x86_64-simulator
+generate_framework $framework_name ios-arm64-arm64e
+generate_framework $framework_name xros-arm64-x86_64-simulator
+
+echo "BacklightServices.xcframework has been generated successfully!"
---
+++ Example/BLSExample_2024/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  BacklightServicesExample_2024
+//
+//  Created by Kyle on 2025/9/13.
+//
+
+import SwiftUI
+
+@main
+struct BacklightServicesExample_2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
