e8090a2f05b2bba73e7772cde4427b3cd1bf1f0a
Kyle <kyle201817146@gmail.com>
2025-12-27 12:53:19 +0800

    Sync RBPath headers and cleanup RB_REFINED_FOR_SWIFT (#28)

--- RB/2024/RenderBox.xcframework/macos-arm64e-arm64-x86_64/RenderBox.framework/Headers/RBPathStorage.h
+++ RB/2024/RenderBox.xcframework/macos-arm64e-arm64-x86_64/RenderBox.framework/Headers/RBPathStorage.h
@@ -16,52 +16,41 @@ RB_ASSUME_NONNULL_BEGIN
 RB_EXTERN_C_BEGIN
 
 RB_EXPORT
-RB_REFINED_FOR_SWIFT
 void RBPathStorageInit(RBPathStorageRef dst, uint32_t capacity, RBPathStorageRef _Nullable source) RB_SWIFT_NAME(RBPathStorageRef.initialize(self:capacity:source:));
 
 RB_EXPORT
-RB_REFINED_FOR_SWIFT
 void RBPathStorageDestroy(RBPathStorageRef storage) RB_SWIFT_NAME(RBPathStorageRef.destroy(self:));
 
 RB_EXPORT
-RB_REFINED_FOR_SWIFT
 void RBPathStorageClear(RBPathStorageRef storage) RB_SWIFT_NAME(RBPathStorageRef.clear(self:));
 
 RB_EXPORT
-RB_REFINED_FOR_SWIFT
-void RBPathStorageAppendElement(RBPathStorageRef storage, RBPathElement element, CGFloat const * points, const void * _Nullable userInfo) RB_SWIFT_NAME(RBPathStorageRef.append(self:element:points:userInfo:));
+bool RBPathStorageAppendElement(RBPathStorageRef storage, RBPathElement element, CGFloat const * points, const void * _Nullable userInfo) RB_SWIFT_NAME(RBPathStorageRef.append(self:element:points:userInfo:));
 
 RB_EXPORT
-RB_REFINED_FOR_SWIFT
-void RBPathStorageAppendPath(RBPathStorageRef, RBPath) RB_SWIFT_NAME(RBPathStorageRef.append(self:path:));
+bool RBPathStorageAppendPath(RBPathStorageRef, RBPath) RB_SWIFT_NAME(RBPathStorageRef.append(self:path:));
 
 RB_EXPORT
-RB_REFINED_FOR_SWIFT
 bool RBPathStorageApplyElements(RBPathStorageRef, void *info, RBPathApplyCallback _Nullable callback) RB_SWIFT_NAME(RBPathStorageRef.apply(self:info:callback:));
 
 RB_EXPORT
-RB_REFINED_FOR_SWIFT
 bool RBPathStorageIsEmpty(RBPathStorageRef storage) RB_SWIFT_NAME(getter:RBPathStorageRef.isEmpty(self:));
 
 RB_EXPORT
-RB_REFINED_FOR_SWIFT
 bool RBPathStorageEqualToStorage(RBPathStorageRef lhs, RBPathStorageRef rhs) RB_SWIFT_NAME(RBPathStorageRef.isEqual(self:to:));
 
 RB_EXPORT
-RB_REFINED_FOR_SWIFT
 bool RBPathStorageIsSingleElement(RBPathStorageRef storage) RB_SWIFT_NAME(getter:RBPathStorageRef.isSingleElement(self:));
 
 RB_EXPORT
-RB_REFINED_FOR_SWIFT
 uint32_t RBPathStorageGetBezierOrder(RBPathStorageRef storage) RB_SWIFT_NAME(getter:RBPathStorageRef.bezierOrder(self:));
 
 RB_EXPORT
-RB_REFINED_FOR_SWIFT
 CGRect RBPathStorageGetBoundingRect(RBPathStorageRef storage) RB_SWIFT_NAME(getter:RBPathStorageRef.boundingRect(self:));
 
 RB_EXPORT
-RB_REFINED_FOR_SWIFT
-CGPathRef RBPathStorageGetCGPath(RBPathStorageRef storage) RB_SWIFT_NAME(getter:RBPathStorageRef.cgPath(self:));
+CF_RETURNS_NOT_RETAINED
+__nullable CGPathRef RBPathStorageGetCGPath(RBPathStorageRef storage) RB_SWIFT_NAME(getter:RBPathStorageRef.cgPath(self:));
 
 RB_EXTERN_C_END
 
