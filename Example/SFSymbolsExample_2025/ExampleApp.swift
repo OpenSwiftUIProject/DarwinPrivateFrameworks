596cf2ab69bc87576160098bb69e816cd8e65be7
Yanan Li <1474700628@qq.com>
2026-03-03 19:13:59 +0800

    Add example

---
+++ Example/SFSymbolsExample_2025/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  DarwinPrivateFrameworks
+//
+//  Created by Yanan Li on 2026/3/3.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
