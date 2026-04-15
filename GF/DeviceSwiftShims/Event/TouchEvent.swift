b0cd69e9c3b50e2cccd888523e114f200b220355
Kyle <kyle201817146@gmail.com>
2026-04-16 03:57:48 +0800

    Add Event API (#56)

---
+++ GF/DeviceSwiftShims/Event/TouchEvent.swift
@@ -0,0 +1,24 @@
+//
+//  TouchEvent.swift
+//  Gestures
+//
+//  Audited for 9126.1.5
+//  Status: Complete
+
+import CoreGraphics
+
+// MARK: - TouchEvent
+
+public struct TouchEvent: SpatialEvent, NestedCustomStringConvertible, Sendable {
+    public var id: EventID
+    public var phase: EventPhase
+    public var timestamp: Timestamp
+    public var location: CGPoint
+
+    public init(id: EventID, phase: EventPhase, timestamp: Timestamp, location: CGPoint) {
+        self.id = id
+        self.phase = phase
+        self.timestamp = timestamp
+        self.location = location
+    }
+}
